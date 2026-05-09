50e1e6d40be44d7c2a25f26664a1d9c82317fabd
Tu Nombre RealLuiggijaviermalaquita9@gmail <TU_EMAIL@ejemplo.com>
2026-05-08 21:53:01 -0400

    correcion 3

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
@@ -17,7 +18,7 @@ export default defineConfig(({ command, mode }) => {
       port: 3000,
       host: '0.0.0.0',
     },
-    plugins: [react()],
+    plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
     },
