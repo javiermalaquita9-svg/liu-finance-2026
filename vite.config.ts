836a2daa7a1b4a36df29e680cca997ed0ead1846
Tu Nombre RealLuiggijaviermalaquita9@gmail <TU_EMAIL@ejemplo.com>
2026-05-08 22:01:50 -0400

    1

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
@@ -18,7 +17,7 @@ export default defineConfig(({ command, mode }) => {
       port: 3000,
       host: '0.0.0.0',
     },
-    plugins: [react(), tailwindcss()],
+    plugins: [react()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
     },
